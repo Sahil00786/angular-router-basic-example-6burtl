50b6b6613e0dc3bcb0fa7da041faef1f18489fb2
Sahil00786 <78851554+Sahil00786@users.noreply.github.com>
2022-04-25 19:53:13 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -21,18 +21,3 @@ import { Component } from '@angular/core';
 export class AppComponent  {
 
 }
-
-/** 
- * EXERCISE 1
- * Duration: 5/10 mins
- * 
- * Add a new route `/users` and display a text into the route component
- * 
- */
-
-/** 
- * EXERCISE 2
- * Duration: 10/15 mins
- * 
- * Create a '<nav-bar>' component and move the menu there
- */
